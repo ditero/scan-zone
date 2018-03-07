620a0e571326c82ee481d798e7efc3929d67d4c5
Gareth <williams.gareth0@gmail.com>
2018-03-07 17:05:41 +0200

    modified the mapper

--- src/js/mapper/jde.data.mapper.js
+++ src/js/mapper/jde.data.mapper.js
@@ -1,37 +1,72 @@
 const Mapper = () => {
-    getPoData = async () => await GetPoForm().getPo();
-    getItemData = async () => await GetItem().getItem();
+    getPoData = async() => await GetPoForm().getPo();
+    getItemData = async() => await GetItem().getItem();
 
-    poMapper = async (jdeForm) => {
+    poMapper = async() => {
         let data = await getPoData();
+        console.log(data)
         let rowset = data['fs_P43081_W43081A'].data.gridData.rowset;
-        let PoForm = {};
-        let processNewForm = new Promise(async (resolve, reject) => {
-            await rowset.forEach(form => {
-                let keys = Object.keys(form);
-                keys.forEach(key => {
-                    // If Need Be Add More Values
-                    let id = form[key].id;
-                    let title = form[key].title ? true : false;
-                    let value = form[key].value;
-                    let internalValue = form[key].internalValue;
+        let titles = data['fs_P43081_W43081A'].data.gridData.titles;
+
+        let newKeys = {};
+        let newObj = {};
+
+        let processNewForm = new Promise(async(resolve, reject) => {
+            for (var title in titles) {
+                let cTitle = titles[title].split(" ").join("");
+                newKeys[cTitle] ? undefined : newKeys[cTitle] = "";
+            }
+
+            let poForm = [];
+
+            function replaceAll(str, find, replace) {
+                let manip = str.replace(new RegExp(find, 'g'), replace);
+                return manip;
+            }
+
+            for (var x = 0; x < rowset.length; x++) {
+                cRow = rowset[x];
+
+                let keys = Object.keys(cRow);
+                let stringRowset = JSON.stringify(cRow);
+                let replaceVal = "";
+
+                keys.forEach(async key => {
+                    let title = cRow[key].title;
 
                     if (title) {
-                        let newTitle = form[key].title.split(" ").join("");
-                        PoForm[newTitle] ? undefined : PoForm[newTitle] = { id, value, internalValue };
+                        let newTitle = JSON.stringify(title.split(" ").join(""));
+                        let cKey = JSON.stringify(key);
+
+                        replaceVal = replaceAll(stringRowset, cKey, newTitle);
                     }
-                });
-            });
-            resolve(true);
+                })
+
+                let parseRowset = JSON.parse(replaceVal);
+
+                poForm.push(parseRowset)
+            }
+
+
+            // let newO = {}
+            // let n = 0
+
+            console.log(poForm)
+                // newObj[index] ? undefined : newObj[index] = row;
         })
-        processNewForm.then((state) => {console.log(PoForm); return PoForm});
+
+        // console.log(rowset)
+
+        // processNewForm.then((state) => { console.log(PoForm); return PoForm });
+
+
     }
 
-    itemMapper = async () => {
+    itemMapper = async() => {
         let data = await getItemData();
         let rowset = data['fs_P43081_W43081B'].data.gridData.rowset;
         let Item = {};
-        let processNewItems = new Promise(async (resolve, reject) => {
+        let processNewItems = new Promise(async(resolve, reject) => {
             await rowset.forEach(item => {
                 let keys = Object.keys(item);
                 keys.forEach(key => {
@@ -49,7 +84,7 @@ const Mapper = () => {
             });
             resolve(true);
         })
-        processNewItems.then((state) => {console.log(Item)});
+        processNewItems.then((state) => { console.log(Item) });
     }
 
     return {
@@ -58,26 +93,11 @@ const Mapper = () => {
     }
 }
 
-const myFunc = function () {
+const myFunc = function() {
 
     let myP = Mapper().poMapper();
-    let myI = Mapper().itemMapper();
+    // let myI = Mapper().itemMapper();
 
 }
 
 myFunc();
-
-
-// if (form.fs_P43081_W43081A) {
-//     rowSet = form.fs_P43081_W43081A.data.gridData.rowset;
-//     rowSet.forEach(item => {
-//         if (item.mnOrderNumber_22.internalValue == Number(PO)) {
-//             resolve(item);
-//         } else {
-//             resolve(false);
-//         }
-//     });
-// } else {
-//     console.log('did not find the form')
-//     resolve(false);
-// }

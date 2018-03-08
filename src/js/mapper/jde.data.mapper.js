98243aeec3ab3eb3d999981328fe12116e9b838e
Gareth <williams.gareth0@gmail.com>
2018-03-08 14:15:04 +0200

    updated the mapper func

--- src/js/mapper/jde.data.mapper.js
+++ src/js/mapper/jde.data.mapper.js
@@ -2,102 +2,120 @@ const Mapper = () => {
     getPoData = async() => await GetPoForm().getPo();
     getItemData = async() => await GetItem().getItem();
 
-    poMapper = async() => {
-        let data = await getPoData();
-        console.log(data)
-        let rowset = data['fs_P43081_W43081A'].data.gridData.rowset;
-        let titles = data['fs_P43081_W43081A'].data.gridData.titles;
+    jdeMapper = async formIdentifier => {
+        let formId = formIdentifier.charAt(formIdentifier.length - 1);
+        let data;
 
-        let newKeys = {};
-        let newObj = {};
+        switch (formId) {
+            case "A":
+                data = await getPoData();
+                break;
+            case "B":
+                data = await getItemData();
+            default:
+                break;
+        }
 
+
+        let rowset = data[formIdentifier].data.gridData.rowset;
         let processNewForm = new Promise(async(resolve, reject) => {
-            for (var title in titles) {
-                let cTitle = titles[title].split(" ").join("");
-                newKeys[cTitle] ? undefined : newKeys[cTitle] = "";
-            }
-
             let poForm = [];
 
-            function replaceAll(str, find, replace) {
-                let manip = str.replace(new RegExp(find, 'g'), replace);
-                return manip;
-            }
-
+            // loop through the key value pairs data
             for (var x = 0; x < rowset.length; x++) {
+                // save the current row
                 cRow = rowset[x];
 
+                // get the keys of the current row
                 let keys = Object.keys(cRow);
+
+                // stringify the current
                 let stringRowset = JSON.stringify(cRow);
-                let replaceVal = "";
+                let replaceVal = stringRowset;
 
-                keys.forEach(async key => {
-                    let title = cRow[key].title;
-
-                    if (title) {
-                        let newTitle = JSON.stringify(title.split(" ").join(""));
-                        let cKey = JSON.stringify(key);
-
-                        replaceVal = replaceAll(stringRowset, cKey, newTitle);
-                    }
-                })
-
-                let parseRowset = JSON.parse(replaceVal);
-
-                poForm.push(parseRowset)
-            }
-
-
-            // let newO = {}
-            // let n = 0
-
-            console.log(poForm)
-                // newObj[index] ? undefined : newObj[index] = row;
-        })
-
-        // console.log(rowset)
-
-        // processNewForm.then((state) => { console.log(PoForm); return PoForm });
-
-
-    }
-
-    itemMapper = async() => {
-        let data = await getItemData();
-        let rowset = data['fs_P43081_W43081B'].data.gridData.rowset;
-        let Item = {};
-        let processNewItems = new Promise(async(resolve, reject) => {
-            await rowset.forEach(item => {
-                let keys = Object.keys(item);
+                // loop through each key
                 keys.forEach(key => {
-                    // If Need Be Add More Values
-                    let id = item[key].id;
-                    let title = item[key].title ? true : false;
-                    let value = item[key].value;
-                    let internalValue = item[key].internalValue;
+                    // get the title of the current row
+                    let title = cRow[key].title;
+                    let value = cRow[key].value;
 
                     if (title) {
-                        let newTitle = item[key].title.split(" ").join("");
-                        Item[newTitle] ? undefined : Item[newTitle] = { id, value, internalValue };
+                        let newKey = title.split(" ").join("");
+                        let cKey = key;
+
+                        // call replaceAll func and pass in the stringified rowset and current key and the new key
+                        replaceVal = replaceAll(replaceVal, cKey, newKey);
                     }
                 });
-            });
-            resolve(true);
-        })
-        processNewItems.then((state) => { console.log(Item) });
-    }
+                let parseRowset = JSON.parse(replaceVal);
+
+                poForm.push(parseRowset);
+            }
+
+            // set poForm to the returned form
+            poForm = createKeyValPairs(poForm);
+
+            // Function to find and replace all the keys in jde stringified data
+            function replaceAll(str, find, replace) {
+                let newStr = str.replace(new RegExp(find, "g"), replace);
+                return newStr;
+            }
+
+            //Transform poForm data to key value pairs
+            function createKeyValPairs(form) {
+                //get keys of form param
+                let keys = Object.keys(form);
+
+                let keyValForm = [];
+
+                for (var i = 0; i < form.length; i++) {
+                    let currentRow = form[i];
+
+                    let keys = Object.keys(currentRow);
+
+                    keys.forEach(key => {
+                        // Remove MOExist 
+                        switch (currentRow[key]) {
+                            case false:
+                                delete currentRow[key];
+                            case true:
+                                delete currentRow[key];
+                            default:
+                                break;
+                        }
+
+                        // get current value of current row
+                        // delete the keys that doesn't have the value key prop to it
+                        let currentValue = currentRow[key];
+                        if (currentValue && typeof currentValue !== "number") {
+                            let currentKey = Object.keys(currentValue);
+                            currentKey.forEach(cKey => {
+                                if (cKey !== "value") {
+                                    delete currentRow[key][cKey];
+                                }
+                            });
+                        }
+                    });
+                    keyValForm.push(currentRow);
+                }
+                return keyValForm;
+            }
+            resolve(poForm);
+        });
+
+        processNewForm.then(newForm => {
+            console.log(newForm);
+            return newForm;
+        });
+    };
 
     return {
-        poMapper,
-        itemMapper
-    }
-}
+        jdeMapper
+    };
+};
 
 const myFunc = function() {
-
-    let myP = Mapper().poMapper();
-    // let myI = Mapper().itemMapper();
-
-}
+    let myP = Mapper().jdeMapper("fs_P43081_W43081A");
+};
 
 myFunc();
